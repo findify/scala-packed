73ab30c529407dbefad66529e61b3f3c97786ac6
Grebennikov Roman <grv@dfdx.me>
2017-07-24 15:43:31 +0300

    add simple open-addressing map

--- core/src/main/scala/io/findify/scalapacked/StructMapImpl.scala
+++ core/src/main/scala/io/findify/scalapacked/StructMapImpl.scala
@@ -1,8 +1,41 @@
 package io.findify.scalapacked
 
-class StructMapImpl(val bucketCount: Int) {
-  val keyHashes = new Array[Int](bucketCount)
-  val valueOffsets = new Array[Int](bucketCount)
+import io.findify.scalapacked.pool.{HeapPool, MemoryPool}
 
-  def put(key: Array[Byte], value: Array[Byte]): Unit = ???
+class StructMapImpl[A, B](val bucketCount: Int, pool: MemoryPool = new HeapPool(1024))(implicit ke: Encoder[A], kd: Decoder[A], ve: Encoder[B], vd: Decoder[B]) {
+  val buckets = new Array[Int](bucketCount)
+  val usedBuckets = new Array[Byte](bucketCount)
+  var count = 0
+
+  private def findBucket(key: A): Int = {
+    var i = key.hashCode() % bucketCount
+    while ((usedBuckets(i) == 1) && (key != kd.read(pool, buckets(i)))) {
+      i = (i + 1) % bucketCount
+    }
+    i
+  }
+
+  def put(key: A, value: B): Unit = {
+    val offset = ke.write(key, pool)
+    ve.write(value, pool)
+    val bucket = findBucket(key)
+    if (usedBuckets(bucket) == 1) {
+      buckets(bucket) = offset
+    } else {
+      buckets(bucket) = offset
+      usedBuckets(bucket) = 1
+      count += 1
+    }
+  }
+
+  def get(key: A): Option[B] = {
+    val bucket = findBucket(key)
+    if (usedBuckets(bucket) == 1) {
+      val keySize = kd.size(pool, buckets(bucket))
+      Some(vd.read(pool, buckets(bucket) + keySize))
+    } else {
+      None
+    }
+
+  }
 }
---
+++ core/src/test/scala/io/findify/scalapacked/StructMapImplTest.scala
@@ -0,0 +1,20 @@
+package io.findify.scalapacked
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class StructMapImplTest extends FlatSpec with Matchers {
+  import api._
+  "map" should "add elements" in {
+    val map = new StructMapImpl[String,String](1024)
+    map.put("foo", "bar")
+    map.get("foo") shouldBe Some("bar")
+  }
+
+  it should "deal with collisions" in {
+    val map = new StructMapImpl[String,String](1024)
+    Range(1,512).foreach(i => map.put(i.toString, i.toString))
+    val results = Range(1,512).map(i => map.get(i.toString).get)
+    results.size shouldBe 511
+    Range(1,512).zip(results).foreach(x => x._1.toString shouldBe x._2)
+  }
+}
